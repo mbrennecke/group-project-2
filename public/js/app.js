3e6b8e3500ce9facb7645c1575e8e7078c6d48da
GGA <41163962+geoffreygo@users.noreply.github.com>
2018-09-17 13:28:44 -0600

    seeds.sql

--- public/js/app.js
+++ public/js/app.js
@@ -26,5 +26,41 @@ $(function() {
       }
     }
   });
+  $('#calendar').fullCalendar('renderEvents', [
+    {
+      title: 'dynamic event one',
+      allDay: false,
+      start: '20180917T120000Z',
+      end: '20180917T130000Z',
+      overlap: false,
+      color: 'red',
+      textColor: 'white'
+    },
+    {
+      title: 'dynamic event two',
+      allDay: false,
+      start: '20180916T120000Z',
+      end: '20180916T130000Z',
+      overlap: false,
+      color: 'green',
+      textColor: 'white'
+    },{
+      title: 'dynamic event three',
+      allDay: false,
+      start: '20180918T120000Z',
+      end: '20180918T130000Z',
+      overlap: false,
+      color: 'blue',
+      textColor: 'white'
+    },{
+      title: 'dynamic event four',
+      allDay: false,
+      start: '20180917T100000Z',
+      end: '20180917T110000Z',
+      overlap: false,
+      color: 'red',
+      textColor: 'white'
+    }
+  ]);
 
 });
