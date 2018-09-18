a387c72ce5c36e72ab76617e19568228be753341
MBrennecke <MBrennecke10@gmail.com>
2018-09-18 11:36:16 -0600

    object created

--- public/js/app.js
+++ public/js/app.js
@@ -64,3 +64,46 @@ $(function() {
   ]);
 
 });
+
+
+$(document).on("click", "#newProviderForm", function(event){
+	event.preventDefault();
+	var firstname = $("#firstname").val().trim();
+	var lastname = $("#lastname").val().trim();
+	var email = $("#email").val().trim();
+	var phone = $("#phone").val().trim();
+	phone = phone.replace(/[^\d]/g, ''); 
+	var address = $("#address1").val().trim();
+	address += " " + $("#address2").val().trim();
+	var city = $("#city").val().trim();
+	var state = $("#state").val().trim();
+	var zip = $("#zip").val().trim();
+	var dow = [];
+	for (var i=1;i<8;i++){
+		var checkbox = "#" + "checkbox" + i;
+		if($(checkbox).is(':checked')){
+		var day = $(checkbox).val();
+		day = parseInt(day.slice(-1));
+		dow.push(day);
+		}
+	}
+	var starttime = $("#start").val().trim();
+	var endtime = $("#start").val().trim();
+	
+	var newObj = {
+		firstname: firstname,
+		lastname: lastname,
+		email: email,
+		phone: phone,
+		address: address,
+		city: city,
+		state: state,
+		zip: zip,
+		dow: dow,
+		start: starttime,
+		end: endtime
+	}
+	
+	console.log(newObj);
+	
+});
