1a2ed9d274f94015322bd22b2651d7291861d0d6
GGA <41163962+geoffreygo@users.noreply.github.com>
2018-09-19 13:55:21 -0600

    adding jwt authentication

--- models/providers.js
+++ models/providers.js
@@ -1,15 +1,15 @@
 module.exports = function(sequelize, DataTypes) {
   var providers = sequelize.define("providers", {
-    providerBusinessName: DataTypes.STRING,
-    repFirstName: DataTypes.STRING,
-    repLastName: DataTypes.STRING,
-    providerEmail: DataTypes.STRING,
-    providerPhone: DataTypes.STRING,
-    providerAddress: DataTypes.STRING,
-    providerCity: DataTypes.STRING,
-    providerState: DataTypes.STRING,
-    providerZip: DataTypes.STRING,
-    businessHours: DataTypes.STRING(1234)
+    providerBusinessName: { type: DataTypes.STRING, allowNull: false},
+    repFirstName: { type: DataTypes.STRING, allowNull: false},
+    repLastName: { type: DataTypes.STRING, allowNull: false},
+    providerEmail: { type: DataTypes.STRING, allowNull: false},
+    providerPhone: { type: DataTypes.STRING, allowNull: false},
+    providerAddress: { type: DataTypes.STRING, allowNull: false},
+    providerCity: { type: DataTypes.STRING, allowNull: false},
+    providerState: { type: DataTypes.STRING, allowNull: false},
+    providerZip: { type: DataTypes.STRING, allowNull: false},
+    businessHours: { type: DataTypes.STRING(1234), allowNull: false}
   });
   providers.associate = function(models) {
     providers.hasMany(models.events, {
