190560010a6aecd9e7539e19d3fd969f82000144
GGA <41163962+geoffreygo@users.noreply.github.com>
2018-09-20 17:45:16 -0600

    working code

--- public/js/verifyToken.js
+++ public/js/verifyToken.js
@@ -7,6 +7,7 @@ function verifyToken(req, res, next) {
   if (!token)
     return res.status(403).send({ auth: false, message: 'No token provided.' });
   jwt.verify(token, config.secret, function(err, decoded) {
+      console.log(token);
     if (err)
     return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     // if everything good, save to request for use in other routes
